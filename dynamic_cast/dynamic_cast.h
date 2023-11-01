9856a585fbd92d34fe21447a7d89212a03c84bfb
ClaudomiroSales <claudomiro.sales@gmail.com>
2023-11-01 11:10:49 -0300

    Create dynamic_cast.h
    Exemplo de dynamic_cast

---
+++ dynamic_cast/dynamic_cast.h
@@ -0,0 +1,43 @@
+#include <iostream>
+using std::cout;
+
+class Item {
+public:
+    virtual void use() = 0;
+};
+
+class Weapon : public Item {
+public:
+    void use() override {
+        cout << "Using a weapon" << std::endl;
+    }
+};
+
+class Potion : public Item {
+public:
+    void use() override {
+        cout << "Using a potion" << std::endl;
+    }
+};
+
+void processItem(Item* item) {
+    if (Weapon* weapon = dynamic_cast<Weapon*>(item)) {
+        // Handle a Weapon
+        weapon->use();
+    } else if (Potion* potion = dynamic_cast<Potion*>(item)) {
+        // Handle a Potion
+        potion->use();
+    } else {
+        std::cout << "Unknown item type" << std::endl;
+    }
+}
+
+int main() {
+    Weapon weapon;
+    Potion potion;
+
+    processItem(&weapon);  // Output: Using a weapon
+    processItem(&potion);  // Output: Using a potion
+
+    return 0;
+}
